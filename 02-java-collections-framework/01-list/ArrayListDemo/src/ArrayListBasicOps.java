5f8c74457498cc068754d0325bdf2a22b9c681e6
Eng-YasminKotb <ysysys141516@gmail.com>
2026-03-25 01:24:11 +0200

    refactor: split ArrayListDemo into separate files per concept

---
+++ 02-java-collections-framework/01-list/ArrayListDemo/src/ArrayListBasicOps.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        ArrayList<String> languages= new ArrayList<String>();
+
+        //Add 5 languages to ArrayList
+        languages.add("Java");
+        languages.add("C++");
+        languages.add("Python");
+        languages.add("C#");
+        languages.add("Go");
+
+        for(String language : languages){
+            System.out.println(language);
+        }
+
+        //Add a language at index 2
+        languages.add(2,"JavaScript");
+        System.out.println("ArrayList size: "+languages.size());
+        // 0    1    2          3     4 5
+        //Java C++ JavaScript Python C# Go
+
+        //Remove one language by index
+        languages.remove(5);
+        System.out.println("ArrayList size after removing one element by index: "+ languages.size());
+
+        //Remove one language by value
+        languages.remove("C#");
+        System.out.println("ArrayList size after removing one element by value: "+ languages.size());
+
+
+        //Check if the list contains a language
+        int idx = languages.indexOf("Python");
+        if (idx != -1) {
+            languages.remove(idx);
+            System.out.println("Language is removed");
+        } else {
+            System.out.println("Language does not exist!");
+        }
+
+
+
+
+    }
+}
