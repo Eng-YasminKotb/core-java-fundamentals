8ec1983c70ccfd494e881ae9b3a29a7ddb9663df
Eng-YasminKotb <ysysys141516@gmail.com>
2026-03-30 02:49:02 +0200

    feat(iteration): using Iterator interface to remove some elemnts based onn condition

--- 02-java-collections-framework/01-list/ArrayListDemo/src/ArrayListSortAndIterate.java
+++ 02-java-collections-framework/01-list/ArrayListDemo/src/ArrayListSortAndIterate.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 
 public class ArrayListSortAndIterate {
     public static void main(String[] args) {
+        //task 5
 
         //Sort a list of names alphabetically, then reverse it — without writing your own sorting/reversing logic
         ArrayList<String> names= new ArrayList<String>();
@@ -20,6 +22,36 @@ public class ArrayListSortAndIterate {
         Collections.reverse(names);
 
         System.out.println("-----revesesd!-----");
+        for (String name : names)
+            System.out.println(name);
+
+        //task6
+        //Iterate the list and remove every name that starts with the letter "A" — do this safely (hint: naive for-each will throw an exception)
+        names.add("Asia");
+
+        System.out.println("-----Before remove names start with 'A'-----");
+
+        for (String name : names)
+            System.out.println(name);
+
+        //❌This will through an exception -IndexOutOfBoundsException-
+        /*for (int i=0;i<=names.size();i++){
+            String name = names.get(i);
+            if (name.charAt(0) == 'A'){
+                names.remove(name);
+            }
+        }*/
+
+        System.out.println("-----remove names start with 'A'-----");
+
+        //💡 suggested solution1
+        Iterator<String> it=names.iterator();
+        while (it.hasNext()){
+            String name = it.next();
+            if (name.charAt(0) == 'A'){
+                it.remove();
+            }
+        }
         for (String name : names)
             System.out.println(name);
 
